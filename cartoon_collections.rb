2b762c490babce37035620468daf86fded2be960
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-25 22:13:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(arr)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  new = arr.map{ |n| "#{n}!"
 end
 
 def long_planeteer_calls# code an argument here
