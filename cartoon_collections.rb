0650d0bd859ef4dd131aed6804964450c6580f80
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-25 22:36:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(arr)
   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
+  arr.include?{ 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
